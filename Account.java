ee887dca64b6750d8e49afc3a7e7ee251ddc7195
teddy-png <78939659+teddy-png@users.noreply.github.com>
2021-04-23 18:47:58 -0400

    Update Account.java

--- Account.java
+++ Account.java
@@ -1,21 +1,7 @@
-
 public class Account {
 
 	/*
-		Issues: 
-			- Unsure of how to create a unique string for any account created within class, there needs to be data held somewhere with program
-			of all the ID's that are taken to be checked against.
-			- Double value for balance is currently unformatted, we can just always format the output to two decimal places, or I can try and create a method
-			that will round the double down to two places whenever it's stored
-			- Unsure which constructors we will need and which we won't
-			- Deleting account would probably be easier to down in a main class which handles account objects
-			- Reminders would most likely make sense to be done in another outside program that handles account objects. Objects can't send information without being
-			called methods, and are program doesn't have event handlers and event streams. Reminders, both for a negative account balance, and for other reminders like
-			transfers and withdrawals, will need to be done outside the class
-			- For a negative account balance, I wrote it as a test method for whether or not the account balance is negative. This can be called
-			from the main program whenever a check is needed. I overloaded the method as well so it can be called from within the other methods of the class for warnings
-			about withdrawals and transfers. The alert window box will prompting the user with a warning and giving them the option to continue or not. 
-			(Or just stop their action completely depending on what we want to do with it)
+
 			
 	*/
 	
